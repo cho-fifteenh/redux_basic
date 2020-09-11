51b009637eebf53d21bb5abcee51c3e453f17a6b
cho-fifteenh <cho@fifteenh.io>
2020-09-11 18:28:19 +0900

    add_container

--- src/components/AddNumber.jsx
+++ src/components/AddNumber.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import store from "../store";
 
 class AddNumber extends Component {
   state = { size: 1 };
@@ -11,8 +10,7 @@ class AddNumber extends Component {
           type="button"
           value="+"
           onClick={function () {
-            //부모에 size와 함께 클릭이벤트 전달
-            store.dispatch({ type: "INCREMENT", size: this.state.size });
+            this.props.onClick(this.state.size);
           }.bind(this)}
         />
         <input
--- src/components/DisplayNumber.jsx
+++ src/components/DisplayNumber.jsx
@@ -1,22 +1,11 @@
 import React, { Component } from "react";
-import store from "../store";
 
 class DisplayNumber extends Component {
-  //store값 가져오기
-  state = { number: store.getState().number };
-  constructor(props) {
-    super(props);
-    store.subscribe(
-      function () {
-        this.setState({ number: store.getState().number });
-      }.bind(this)
-    );
-  }
   render() {
     return (
       <div>
         <h1>DisplayNumber</h1>
-        <input type="text" value={this.state.number} readOnly />
+        <input type="text" value={this.props.number} readOnly />
       </div>
     );
   }
